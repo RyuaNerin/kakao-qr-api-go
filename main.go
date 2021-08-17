7d272b07cf3e0948baacb271d722558831c3c133
RyuaNerin <admin@ryuar.in>
2021-08-17 13:54:19 +0900

    새로운 인증 시스템에 맞게 업데이트

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
-	"regexp"
 	"strconv"
 	"strings"
 	"sync"
@@ -27,7 +26,7 @@ import (
 )
 
 const (
-	UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 KAKAOTALK 9.0.3"
+	UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 KAKAOTALK 9.4.2"
 
 	DefaultPngSize = 256
 
@@ -39,12 +38,11 @@ type Config struct {
 	KakaoPw string `json:"kakao-pw"`
 	ApiKey  string `json:"api-key"`
 	Bind    string `json:"bind"`
+	Proxy   string `json:"proxy"`
 }
 
 var (
 	cfg Config
-
-	regToken = regexp.MustCompile(`"token":\s*"(.+?)"`)
 
 	tokenCache        string
 	tokenCachePng     = make(map[int][]byte)
@@ -61,9 +59,19 @@ func main() {
 	panicn(err)
 	defer os.RemoveAll(dpUserDir)
 
+	if cfg.Proxy != "" {
+		if tcpProxy, err := net.DialTimeout("tcp", cfg.Proxy, time.Second); err == nil {
+			tcpProxy.Close()
+
+			proxyURL, _ := url.Parse("http://" + cfg.Proxy)
+			httpClient.Transport = &http.Transport{
+				Proxy: http.ProxyURL(proxyURL),
+			}
+		}
+	}
+
 	// Init Chromium
-	dpContext, cancel := chromedp.NewExecAllocator(
-		context.Background(),
+	dpOpts := []chromedp.ExecAllocatorOption{
 		chromedp.UserDataDir(dpUserDir),
 		chromedp.UserAgent(UserAgent),
 		chromedp.WindowSize(375, 667),
@@ -71,7 +79,11 @@ func main() {
 		chromedp.NoDefaultBrowserCheck,
 		chromedp.Headless,
 		chromedp.DisableGPU,
-	)
+	}
+	if cfg.Proxy != "" {
+		dpOpts = append(dpOpts, chromedp.ProxyServer(cfg.Proxy))
+	}
+	dpContext, cancel := chromedp.NewExecAllocator(context.Background(), dpOpts...)
 	defer cancel()
 
 	var l net.Listener
@@ -308,19 +320,19 @@ func generateToken(ctx context.Context, doLogin bool) (string, error) {
 		}
 	}
 
-	header := http.Header{
-		"User-Agent": []string{UserAgent},
-	}
-
 	//////////////////////////////////////////////////////////////////////////////////////////
 
 	req, _ := http.NewRequest(
-		"GET",
-		"https://accounts.kakao.com/qr_check_in",
+		"POST",
+		"https://vaccine-qr.kakao.com/api/v1/qr",
 		nil,
 	)
-	req.WithContext(ctx)
-	req.Header = header
+	req.Body = ioutil.NopCloser(strings.NewReader(`{"epURL":null}`))
+	req.Header = http.Header{
+		"User-Agent":   []string{UserAgent},
+		"content-type": []string{"application/json;charset=utf-8"},
+	}
+	req = req.WithContext(ctx)
 
 	res, err := httpClient.Do(req)
 	if err != nil {
@@ -329,46 +341,19 @@ func generateToken(ctx context.Context, doLogin bool) (string, error) {
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("qr_check_in returned %d %s", res.StatusCode, res.Status)
-	}
-
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil && err != io.EOF {
-		return "", err
-	}
-
-	matches := regToken.FindSubmatch(data)
-	if matches == nil {
-		return "", fmt.Errorf("Cannot find token")
-	}
-
-	//////////////////////////////////////////////////////////////////////////////////////////
-
-	req, _ = http.NewRequest(
-		"GET",
-		"https://accounts.kakao.com/qr_check_in/request_qr_data.json?lang=ko&os=ios&webview_v=2&is_under_age=false&token="+string(matches[1]),
-		nil,
-	)
-	req.Header = header
-	req.WithContext(ctx)
-
-	res, err = httpClient.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("qr_check_in/request_qr_data.json returned %d %s", res.StatusCode, res.Status)
+		return "", fmt.Errorf("api/v1/qr returned %d %s", res.StatusCode, res.Status)
 	}
 
 	var qr struct {
-		QRData string `json:"qr_data"`
+		QRData string `json:"qrData"`
 	}
 
 	err = json.NewDecoder(res.Body).Decode(&qr)
 	if err != nil && err != io.EOF {
 		return "", err
+	}
+	if qr.QRData == "" {
+		return "", errors.New("qrData is empty")
 	}
 
 	return qr.QRData, nil
